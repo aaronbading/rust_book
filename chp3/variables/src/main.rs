c565abde4b409c83bb5bfc14533a405e60a9fda9
Aaron Bading <aaronbading.ab@gmail.com>
2023-03-22 14:42:15 -0500

    variables and data types in rust

--- chp3/variables/src/main.rs
+++ chp3/variables/src/main.rs
@@ -10,5 +10,68 @@ fn main() {
 
     println!("The value of x is: {x}");
 
+    let guess: u32 = "42".parse().expect("Not a number!");
+    println!("The value of guess is: {guess}");
+
+    //Floats
+    let _x = 2.0; // f64
+
+    let _y: f32 = 3.0; // f32
+
+    // addition
+    let _sum = 5 + 10;
+
+    // subtraction
+    let _difference = 95.5 - 4.3;
+
+    // multiplication
+    let _product = 4 * 30;
+
+    // division
+    let _quotient = 56.7 / 32.2;
+    let _truncated = -5 / 3; // Results in -1
+
+    // remainder
+    let _remainder = 43 % 5;
+
+    // Boolean
+    let _t = true;
+
+    let _f: bool = false; // with explicit type annotation
+
+    // Character
+
+    let _c = 'z';
+    let _z: char = 'ℤ'; // with explicit type annotation
+    let _heart_eyed_cat = '😻';
+
+    // Tuples
+    let tup = (500, 6.4, 1);
+
+    let (x, y, z) = tup;
+
+    println!("The value of y is: {y}");
+
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let five_hundred = x.0;
+
+    let six_point_four = x.1;
+
+    let one = x.2;
+
+    //Arrays
+
+    let a = [1, 2, 3, 4, 5];
+    let first = a[0];
+    let second = a[1];
+    
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
+
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+
+    let a = [3; 5];  // [3, 3, 3, 3, 3]
+
 
 }
