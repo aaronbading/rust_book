5da31229a8b937557369558876a410becfa1799a
Aaron Bading <aaronbading.ab@gmail.com>
2023-03-28 13:55:00 -0500

    skip last section of this chapter

--- chp9/read_username/src/main.rs
+++ chp9/read_username/src/main.rs
@@ -1,3 +1,54 @@
+
+use std::fs::File;
+use std::io::{self, Read};
+use std::fs;
+use std::io;
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+    
+}
+
+
+fn read_username_from_file_alternative() -> Result<String, io::Error> {
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+fn read_username_from_file_shorter_alternative() -> Result<String, io::Error> {
+    let mut username = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+
+    Ok(username)
+}
+fn read_username_from_file() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
+}
+
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
+}
+
+
 fn main() {
     println!("Hello, world!");
+    let username = read_username_from_file();
+    let username_again = read_username_from_file_alternative();
+    println!("Username: {}", username.unwrap());
+    println!("Username: {}", username_again.unwrap());
 }
