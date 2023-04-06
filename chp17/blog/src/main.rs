3b338575ac2a0fe2f9ac3219d696ef35ee1487db
Aaron Bading <aaronbading.ab@gmail.com>
2023-04-06 14:35:58 -0500

    add blog2 example

--- chp17/blog/src/main.rs
+++ chp17/blog/src/main.rs
@@ -1,5 +1,10 @@
 use blog::Post;
 
+//Addtional stuff to add to the blog crate:
+//Add a reject method that changes the post’s state from PendingReview back to Draft.
+// Require two calls to approve before the state can be changed to Published.
+// Allow users to add text content only when a post is in the Draft state. Hint: have the state object responsible for what might change about the content but not responsible for modifying the Post.
+
 fn main() {
     let mut post = Post::new();
 
