639bd3fcaa32ad2136613d2cad0cf0a5be6c0579
Aaron Bading <aaronbading.ab@gmail.com>
2023-04-07 13:10:56 -0500

    and more examples still :D

---
+++ chp18/example_12/src/main.rs
@@ -0,0 +1,19 @@
+
+fn main() {
+    let numbers = (2, 4, 8, 16, 32);
+
+match numbers {
+    (first, .., last) => {
+        println!("Some numbers: {first}, {last}");
+    }
+
+}
+
+let num = Some(4);
+
+match num {
+    Some(x) if x % 2 == 0 => println!("The number {} is even", x),
+    Some(x) => println!("The number {} is odd", x),
+    None => (),
+}
+}
