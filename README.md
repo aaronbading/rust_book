a76ba31628c329a4a2e85ff9e42a280affb64ced
Aaron Bading <aaronbading.ab@gmail.com>
2023-04-07 12:42:37 -0500

    first few examples in chapter 18 section 3

--- README.md
+++ README.md
@@ -195,3 +195,5 @@ Also discussed were the Deref and Drop traits, which enable a lot of the functio
 # Chapter 17 | OOP features in RUST
 
 - Summary : No matter whether or not you think Rust is an object-oriented language after reading this chapter, you now know that you can use trait objects to get some object-oriented features in Rust. Dynamic dispatch can give your code some flexibility in exchange for a bit of runtime performance. You can use this flexibility to implement object-oriented patterns that can help your code’s maintainability. Rust also has other features, like ownership, that object-oriented languages don’t have. An object-oriented pattern won’t always be the best way to take advantage of Rust’s strengths, but is an available option.
+
+# Chapter 18 | Patterns and Matching
---
+++ chp18/example_5_matching/src/main.rs
@@ -0,0 +1,44 @@
+fn main() {
+
+    //example 1
+    let x = 1;
+
+    match x {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+
+    //example 2 
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched, y = {y}"),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {y}", x);
+
+    // example 3
+    let x = 1;
+
+    match x {
+        1 | 2 => println!("one or two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+
+    // example 5 with chars 
+    let x = 'c';
+
+    match x {
+        'a'..='j' => println!("early ASCII letter"),
+        'k'..='z' => println!("late ASCII letter"),
+        _ => println!("something else"),
+    }
+
+    
+}
