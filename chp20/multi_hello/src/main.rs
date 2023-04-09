adc956de2da7a5374f737d018b64510f59f1e5ff
Aaron Bading <aaronbading.ab@gmail.com>
2023-04-09 12:08:54 -0500

    finish the multi_hello project with graceful exit

--- chp20/multi_hello/src/main.rs
+++ chp20/multi_hello/src/main.rs
@@ -45,3 +45,20 @@ fn handle_connection(mut stream: TcpStream) {
 
     stream.write_all(response.as_bytes()).unwrap();
 }
+
+
+// Run below main to test graceful exit of the server
+// fn main() {
+//     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+//     let pool = ThreadPool::new(4);
+
+//     for stream in listener.incoming().take(2) {
+//         let stream = stream.unwrap();
+
+//         pool.execute(|| {
+//             handle_connection(stream);
+//         });
+//     }
+
+//     println!("Shutting down.");
+// }
