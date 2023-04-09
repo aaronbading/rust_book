1c1487534c6dda56c822b3a69dc763befbf110aa
Aaron Bading <aaronbading.ab@gmail.com>
2023-04-08 22:48:28 -0500

    make server return basic html script

--- chp20/hello/src/main.rs
+++ chp20/hello/src/main.rs
@@ -1,6 +1,7 @@
 use std::{
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    fs,
 };
 
 fn main() {
@@ -13,6 +14,7 @@ fn main() {
     }
 }
 
+
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let http_request: Vec<_> = buf_reader
@@ -22,7 +24,12 @@ fn handle_connection(mut stream: TcpStream) {
         .collect();
     // println!("Request: {:#?}", http_request);
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
 }
